673c2c2176053ec6e9b07cbbe77e3d31233742cb
prakharmodi26 <prakharmodi25@gmail.com>
2026-02-09 16:49:08 -0500

    Project complete

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Jobby",
   description: "Your personal job search & recommendation engine",
+  icons: {
+    icon: "/favcon_jobby.png",
+  },
 };
 
 export default function RootLayout({
